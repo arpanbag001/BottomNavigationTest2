b6ca8f52903d3d0eae301ec28c3725e98d9596ae
Arpan Bag <arpanbag1996@users.noreply.github.com>
2018-05-15 20:28:21 +0530

    Disabled BottomNavigationView animations

--- app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
+++ app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
@@ -30,12 +30,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void setUpBnvAccordingToOrientation(BottomNavigationViewEx bnve, int orientation) {
+
+        bnve.enableAnimation(false);
         bnve.enableShiftingMode(false);
+        bnve.enableItemShiftingMode(false);
 
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             //code for portrait mode
             bnve.setTextVisibility(false);
             bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, 48));
+            bnve.setIconsMarginTop(BottomNavigationViewEx.dp2px(this,12));
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             //code for landscape mode
             bnve.setTextVisibility(true);
