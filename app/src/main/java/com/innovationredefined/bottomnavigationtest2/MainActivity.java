7f194af4971482fc59592d335494c5ef6589f034
Arpan Bag <arpanbag1996@users.noreply.github.com>
2018-05-15 22:35:10 +0530

    Completed setting up ViewPager and Fragments

--- app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
+++ app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
@@ -3,6 +3,7 @@ package com.innovationredefined.bottomnavigationtest2;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,21 +12,39 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationViewEx bnve;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SampleFragmentAdapter fragmentAdapter = new SampleFragmentAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.vp);
+        viewPager.setAdapter(fragmentAdapter);
         bnve = findViewById(R.id.bnve);
-        setUpBnvAccordingToOrientation(bnve,this.getResources().getConfiguration().orientation);
+        setUpBnvAccordingToOrientation(bnve, this.getResources().getConfiguration().orientation);
         bnve.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 bnve.refreshLabelsVisibility();
+                switch (item.getItemId()) {
+                    case R.id.menu_item_one:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.menu_item_two:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.menu_item_three:
+                        viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.menu_item_four:
+                        viewPager.setCurrentItem(3);
+                        break;
+                }
                 return false;
             }
         });
+
 
     }
 
@@ -34,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
         // Checks the orientation of the screen
         setUpBnvAccordingToOrientation(bnve, newConfig.orientation);
-
     }
 
     void setUpBnvAccordingToOrientation(BottomNavigationViewEx bnve, int orientation) {
@@ -47,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             //code for portrait mode
             bnve.setTextVisibility(false);
             bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, 48));
-            bnve.setIconsMarginTop(BottomNavigationViewEx.dp2px(this,12));
+            bnve.setIconsMarginTop(BottomNavigationViewEx.dp2px(this, 12));
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             //code for landscape mode
             bnve.setTextVisibility(true);
