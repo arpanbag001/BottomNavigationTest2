06f40cb6a92e4b1bc9ae694644aa08f35795f5ea
Arpan Bag <arpanbag1996@users.noreply.github.com>
2018-05-15 19:50:47 +0530

    Developing...

--- app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
+++ app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.innovationredefined.bottomnavigationtest2;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,15 +9,38 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationViewEx bnve;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bnve = findViewById(R.id.bnve);
+        setUpBnvAccordingToOrientation(bnve,this.getResources().getConfiguration().orientation);
 
-        BottomNavigationViewEx bnve = findViewById(R.id.bnve);
-        bnve.setTextVisibility(false);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Checks the orientation of the screen
+        setUpBnvAccordingToOrientation(bnve, newConfig.orientation);
+
+    }
+
+    void setUpBnvAccordingToOrientation(BottomNavigationViewEx bnve, int orientation) {
         bnve.enableShiftingMode(false);
-        bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, 48));
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            //code for portrait mode
+            bnve.setTextVisibility(false);
+            bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, 48));
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //code for landscape mode
+            bnve.setTextVisibility(true);
+            //bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, 48));
+        }
+
     }
 }
