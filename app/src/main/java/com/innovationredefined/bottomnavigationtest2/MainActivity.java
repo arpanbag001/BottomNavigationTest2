269bbe1bc4f27361412c5754612c2f9be53fd115
Arpan Bag <arpanbag1996@users.noreply.github.com>
2018-05-15 19:08:06 +0530

    Initial commit

---
+++ app/src/main/java/com/innovationredefined/bottomnavigationtest2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.innovationredefined.bottomnavigationtest2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationViewEx bnve = findViewById(R.id.bnve);
+        bnve.setTextVisibility(false);
+        bnve.enableShiftingMode(false);
+    }
+}
